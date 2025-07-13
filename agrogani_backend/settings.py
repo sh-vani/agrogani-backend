e5118c73fa3207f36b7fd32fef58ae32843c100a
shivaniglobal <Shivanikush2002@gmaial.com>
2025-07-13 06:04:21 +0530

    ✅ Fix: Added mysqlclient for Railway deployment

--- agrogani_backend/settings.py
+++ agrogani_backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
