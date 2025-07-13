15987e199f750f70969f798c5d9cf1d77035498b
shivaniglobal <Shivanikush2002@gmaial.com>
2025-07-13 07:44:03 +0530

    ✅ Fixed DATABASE_URL and added health check

--- agrogani_backend/urls.py
+++ agrogani_backend/urls.py
@@ -16,15 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+def health_check(request):
+    return JsonResponse({'status': 'Backend live 🚀'})
+
 urlpatterns = [
    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+
+  path('', health_check),
 
     path('admin/', admin.site.urls),
     path('api/account/', include('accounts.urls')),
