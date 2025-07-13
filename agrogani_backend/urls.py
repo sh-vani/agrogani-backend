e0560ccaaaa9bb4e596dd64960fa3730d06ee697
shivaniglobal <Shivanikush2002@gmaial.com>
2025-07-13 07:09:20 +0530

    � Cleaned up migration trigger

--- agrogani_backend/urls.py
+++ agrogani_backend/urls.py
@@ -49,5 +49,3 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# in wsgi.py temporarily
-import run_migrate
