c55f44e4f9f80907f7722be04aa7d0840392495a
shivaniglobal <Shivanikush2002@gmaial.com>
2025-07-27 11:24:28 +0530

    agro

--- advisory/views.py
+++ advisory/views.py
@@ -4,6 +4,10 @@ from .serializers import AdvisorySerializer,FertilizerShopSerializer  # Updated 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+
+
+
 # your other imports...
 
 
