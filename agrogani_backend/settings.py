d89222ef84d122e01dc12eecc1581e1e9eb1994b
sh-vani <shivanikush2002@gmail.com>
2025-09-27 18:11:21 +0530

    chanfes

--- agrogani_backend/settings.py
+++ agrogani_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from corsheaders.defaults import default_headers
 from pathlib import Path 
 import pymysql
 pymysql.install_as_MySQLdb()
